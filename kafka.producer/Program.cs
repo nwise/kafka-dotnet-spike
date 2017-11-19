6ce60d746f9744cb3817d8e32a775f09092a26eb
Nathan Wise <nathanwise@gmail.com>
2017-11-19 00:19:06 -0500

    Initial Commit

---
+++ kafka.producer/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using Confluent.Kafka;
+using Confluent.Kafka.Serialization;
+
+namespace kafka.producer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string kafkaEndpoint = "127.0.0.1:9092";
+            string kafkaTopic = "testtopic";
+            int i = 0;
+
+            var producerConfig = new Dictionary<string, object>{ { "bootstrap.servers", kafkaEndpoint}};
+
+            Console.WriteLine("Starting Producer");
+            using(var producer = new Producer<Null, string>(producerConfig, null, new StringSerializer(Encoding.UTF8)))
+            {
+                var cancelled = false;
+                Console.CancelKeyPress += (_, e) => {
+                    e.Cancel = true;
+                    cancelled = true;
+                };
+
+                while (!cancelled)
+                {
+                    DateTime now = DateTime.Now;
+                    var message = $"Event: {now}";
+                    //var message = $"Event {i}";
+                    var result = producer.ProduceAsync(kafkaTopic, null, message).GetAwaiter().GetResult();
+
+                    Console.WriteLine($"{now} Event sent on Partition: {result.Partition}, with Offset: {result.Offset}");
+                    Thread.Sleep(1000);
+                }
+            }
+        }
+    }
+}
+
---
+++ kafka.pubsub.console/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using Confluent.Kafka;
+using Confluent.Kafka.Serialization;
+
+namespace kafka.pubsub.console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var group = args[0];
+            var name = args[1];
+            string kafkaEndpoint = "127.0.0.1:9092";
+            string kafkaTopic = "testtopic";
+
+            var consumerConfig = new Dictionary<string, object>
+            {
+                {"group.id", group},
+                {"metadata.max.age.ms", 1000},
+                {"bootstrap.servers", kafkaEndpoint},
+            };
+
+            Console.WriteLine($"Starting Consumer {group}:{name}");
+            var consumer = new Consumer<Null, string>(consumerConfig, null, new StringDeserializer(Encoding.UTF8));
+            consumer.OnMessage += (obj, msg) =>
+                {
+                    Console.WriteLine($"{group}:{name} Received: {msg.Value}");
+                };
+
+            var cancelled = false;
+            Console.CancelKeyPress += (_, e) => {
+                e.Cancel = true;
+                cancelled = true;
+            };
+
+            consumer.Subscribe(new List<string>() { kafkaTopic });
+
+            while (!cancelled)
+            {
+                consumer.Poll(10);
+            }
+        }
+    }
+}
